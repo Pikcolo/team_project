74636fe350d05101f1ea34c84d3ba89cd23b5ea7
PiriPiri11 <153788607+6610110724@users.noreply.github.com>
2024-03-12 15:05:08 +0700

    Update dash_web.py

--- dash_web.py
+++ dash_web.py
@@ -1,58 +1,251 @@
-from dash import dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
-import dash_mantine_components as dmc
+from dash import Dash, html, dcc, callback, Output, Input
+import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+# Load Data
+data = pd.read_csv("team_project/data_cleaned.csv")
+predict_data = pd.read_csv("team_project/data_cleaned.csv")
 
+# Data Preprocessing
+data["DATETIMEDATA"] = pd.to_datetime(data["DATETIMEDATA"], format="%Y-%m-%d %H:%M:%S")
+data.sort_values("DATETIMEDATA", inplace=True)
+
+# External CSS
 external_stylesheets = [
     {
-        "href": "https://fonts.googleapis.com/css2?"
-        "family=Lato:wght@400;700&display=swap",
+        "href": "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap",
         "rel": "stylesheet",
     },
+    "assets/style.css"  # Link to your CSS file
 ]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('air4thai_44t_stations_data.csv')
+# Initialize the Dash app
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = "Air Quality Analytics"
 
-@app.callback(
-    Output(component_id='graph-placeholder', component_property='figure'),
-    [Input('dummy-input', 'value')]  # Adding a dummy input
-)
-def update_graph(dummy_value):
-    axis_label = px.line(df, x='DATETIMEDATA', y=['PM25'])
-    axis_label.update_layout(
-        xaxis_title="Date and Time",  # X-axis label
-        yaxis_title="PM 2.5",  # Y-axis label
-    )
-    return axis_label
-
-table = dbc.Card(
-    [
-        dash_table.DataTable(data=df.to_dict('records'), page_size=5, style_table={'overflowX': 'auto'})
-    ], color='light', outline=True
-)
-
-line_graph = dbc.Card(
-    [
-        dbc.CardBody([
-            html.H1('Line Graph'),
-            dcc.Graph(figure=update_graph(None), id='graph-placeholder')  # Initial call with None
-        ]), dbc.CardFooter(table)
-    ], color="light", outline=True, class_name="board-curved"
-)
-
+# App layout
 app.layout = html.Div(
-    [
-        html.Div(html.H1('Regression PM 2.5', className='bold-text'), className='dash'),
-        dcc.Input(id='dummy-input', style={'display': 'none'}),  # Dummy input with hidden style
-        dbc.Row([
-            dbc.Col(html.Div(line_graph, className='space-top'), width=5)
-        ], justify='around')
+    children=[
+        html.Div(
+            children=[
+                html.P(children="☁️", className="header-emoji"),
+                html.H1(children="Air Quality Analytics", className="header-title"),
+                html.P(
+                    children="Analyze the air quality data",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Parameter", className="menu-title"),
+                        dcc.Dropdown(
+                            id="parameter-filter",
+                            options=[
+                                {"label": param, "value": param}
+                                for param in data.columns[1:]
+                            ],
+                            value="PM25",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                        ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data["DATETIMEDATA"].min().date(),
+                            max_date_allowed=data["DATETIMEDATA"].max().date(),
+                            start_date=data["DATETIMEDATA"].min().date(),
+                            end_date=data["DATETIMEDATA"].max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="line-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(
+                            className="menu-title"
+                        ),
+                        html.Div(
+                            id="stats-table",
+                            className="stats-table",
+                        ),
+                    ],
+                    className="card",
+                    style={"width": "48%", "float": "right"},  # Add this line
+                ),
+                html.Div(
+                    html.Div(
+                        children=dcc.Graph(id="stats-chart"),
+                    ),
+                    className="card",
+                    style={"width": "50%", "float": "left"},  # Add this line
+                ),
+            ],
+            className="wrapper",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Parameter", className="menu-title"),
+                        dcc.Dropdown(
+                            id="parameter-predict",
+                            options=[
+                                {"label": param, "value": param}
+                                for param in predict_data.columns[1:]
+                            ],
+                            value="PM25",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="predict-chart", config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
     ]
 )
 
+# Callbacks
+@app.callback(
+    Output("line-chart", "figure"),
+    [
+        Input("parameter-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_chart(selected_parameter, start_date, end_date):
+    mask = (
+        (data["DATETIMEDATA"] >= start_date)
+        & (data["DATETIMEDATA"] <= end_date)
+    )
+    filtered_data = data.loc[mask]
+    trace = {
+        "x": filtered_data["DATETIMEDATA"],
+        "y": filtered_data[selected_parameter],
+        "type": "lines",
+        "name": selected_parameter,
+    }
+    layout = {
+        "title": f"{selected_parameter} over Time",
+        "xaxis": {"title": "Datetime"},
+        "yaxis": {"title": selected_parameter},
+        "colorway": ["#29b6f6"],  # or any other color
+    }
+    return {"data": [trace], "layout": layout}
 
-if __name__ == '__main__':
+@app.callback(
+    Output("stats-chart", "figure"),
+    [
+        Input("parameter-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_stats_chart(selected_parameter, start_date, end_date):
+    mask = (
+        (data["DATETIMEDATA"] >= start_date)
+        & (data["DATETIMEDATA"] <= end_date)
+    )
+    filtered_data = data.loc[mask]
+    stats = filtered_data[selected_parameter].describe().reset_index().round(2)
+    stats.columns = ["Statistic", "Value"]
+    fig = px.bar(
+        stats,
+        x="Statistic",
+        y="Value",
+        title=f"Statistics - {selected_parameter} ({start_date}-{end_date})",
+        color="Statistic",
+    ).update_layout(plot_bgcolor="#F5F5F5")
+    return fig
+
+@app.callback(
+    Output("stats-table", "children"),
+    [
+        Input("parameter-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_stats_table(selected_parameter, start_date, end_date):
+    mask = (
+        (data["DATETIMEDATA"] >= start_date)
+        & (data["DATETIMEDATA"] <= end_date)
+    )
+    filtered_data = data.loc[mask]
+    stats = filtered_data[selected_parameter].describe().reset_index().round(2)
+    stats.columns = ["Statistic", "Value"]
+    stats_table = dbc.Table.from_dataframe(stats, striped=True, bordered=True, hover=True, className="custom-table")
+    title = html.Div(children=f"Statistics - {selected_parameter} ({start_date}-{end_date})", className="menu-title")
+    return [title, stats_table]
+
+@app.callback(
+    Output("predict-chart", "figure"),
+    [
+        Input("parameter-predict", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_predict_chart(selected_parameter, start_date, end_date):
+    mask = (
+        (data["DATETIMEDATA"] >= start_date)
+        & (data["DATETIMEDATA"] <= end_date)
+    )
+    filtered_data = predict_data.loc[mask]
+    trace = {
+        "x": filtered_data["DATETIMEDATA"],
+        "y": filtered_data[selected_parameter],
+        "type": "lines",
+        "name": selected_parameter,
+    }
+    layout = {
+        "title": f"{selected_parameter} Prediction for the next week",
+        "xaxis": {"title": "Datetime"},
+        "yaxis": {"title": selected_parameter},
+        "colorway": ["#ec407a"]
+    }
+    return {"data": [trace], "layout": layout}
+
+if __name__ == "__main__":
     app.run_server(debug=True)
